db84f8da63710e4411ab2b5cb82558d74b9a6ce0
raffyxyz <raffy.amoguis1@gmail.com>
2024-04-19 11:14:40 +0800

    chore: home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { Button } from "@mantine/core";
-import { ClientLayout } from "@/layouts/client-layout";
-import { AdminLayout } from "@/layouts/admin-layout";
+import { Text } from "@mantine/core";
 
 export default function Home() {
-  return (
-    <AdminLayout>
-      <Button variant="filled">Button</Button>
-    </AdminLayout>
-  );
+  return <Text>Hello World</Text>;
 }
